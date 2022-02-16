89479b32f6f6fe24ff9358fc7a83aa799308e3ac
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-02-17 02:38:43 +0600

    js working

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const incomeInput = document.getElementById('income-input');
+    const incomeInputValue = incomeInput.value;
+    const foodInput = document.getElementById('food-input');
+
+    const rentInput = document.getElementById('rent-input');
+    const clothesInput = document.getElementById('clothes-input')
+})
