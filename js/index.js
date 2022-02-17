556574a7279be11cfd942d3dfacffce11101d60b
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-02-17 09:48:19 +0600

    initial part done

--- js/index.js
+++ js/index.js
@@ -1,20 +1,39 @@
-function expanceBalanceCalculation() {
+function inputValue() {
     const incomeInput = document.getElementById('income-input');
     const incomeInputValue = incomeInput.value;
+    return incomeInputValue;
+}
+
+function expanceBalanceCalculation() {
+    const incomeInputValue = inputValue();
     const foodInput = document.getElementById('food-input');
     const foodInputValue = foodInput.value;
     const rentInput = document.getElementById('rent-input');
     const rentInputValue = rentInput.value;
     const clothesInput = document.getElementById('clothes-input');
-    const clothesInputValue = clothesInput.value;
-    const expanceTotal = parseFloat(foodInputValue) + parseFloat(rentInputValue) + parseFloat(clothesInputValue);
-    const balanceTotal = parseFloat(incomeInputValue) - expanceTotal;
-
     const expanceTotalDisplay = document.getElementById('total-expance');
-    expanceTotalDisplay.innerText = expanceTotal;
     const balanceTotalDisplay = document.getElementById('total-balance');
-    balanceTotalDisplay.innerText = balanceTotal;
-    return balanceTotal;
+    const clothesInputValue = clothesInput.value;
+
+    if (typeof (parseFloat(incomeInputValue)) == "number" && parseFloat(incomeInputValue) > 0 && typeof (parseFloat(foodInputValue)) == "number" && parseFloat(foodInputValue) > 0 &&
+        typeof (parseFloat(rentInputValue)) == "number" && parseFloat(rentInputValue) > 0 &&
+        typeof (parseFloat(clothesInputValue)) == "number" && parseFloat(clothesInputValue) > 0) {
+
+        const expanceTotal = parseFloat(foodInputValue) + parseFloat(rentInputValue) + parseFloat(clothesInputValue);
+        const balanceTotal = parseFloat(incomeInputValue) - expanceTotal;
+
+
+        expanceTotalDisplay.innerText = expanceTotal;
+        balanceTotalDisplay.innerText = balanceTotal;
+        return balanceTotal;
+    }
+    else {
+
+        expanceTotalDisplay.innerText = 'Please give positive number as an input';
+        balanceTotalDisplay.innerText = "Balance can't be calulated";
+        return balanceTotal = 'No balance';
+    }
+
 }
 
 
@@ -22,9 +41,29 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     expanceBalanceCalculation();
 })
 document.getElementById('save-btn').addEventListener('click', function () {
+
+    const incomeInputValue = inputValue();
     const totalBalance = expanceBalanceCalculation();
     const saveInput = document.getElementById('save-input');
     const saveInputValue = saveInput.value;
-    console.log(saveInputValue);
-    console.log(typeof (saveInputValue));
+    const savingAmountField = document.getElementById('saving-amount');
+    const remainingBalanceField = document.getElementById('remaining-balance');
+
+    if (typeof (parseFloat(saveInputValue) == 'number') && parseFloat(saveInputValue) > 0 &&
+        typeof (parseFloat(totalBalance) == 'number') && parseFloat(totalBalance) > 0 &&
+        typeof (parseFloat(incomeInputValue) == 'number') && parseFloat(incomeInputValue) > 0) {
+
+        const savingAmount = parseFloat(incomeInputValue) * ((parseFloat(saveInputValue) / 100));
+        savingAmountField.innerText = savingAmount;
+
+        const remainingBalance = totalBalance - savingAmount;
+        remainingBalanceField.innerText = remainingBalance;
+    }
+    else {
+        savingAmountField.innerText = 'Give positive number as an input';
+        remainingBalanceField.innerText = 'Remaining balance can not be calculated';
+    }
+
+
+
 })
