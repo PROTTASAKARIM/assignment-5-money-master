667456261ba06ed8f3c64854597cab9cfccafe4f
PROTTASAKARIM <prottasakarim99@gmail.com>
2022-02-17 07:21:29 +0600

    expance function done

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
-document.getElementById('calculate-btn').addEventListener('click', function () {
+function expanceBalanceCalculation() {
     const incomeInput = document.getElementById('income-input');
     const incomeInputValue = incomeInput.value;
     const foodInput = document.getElementById('food-input');
-
+    const foodInputValue = foodInput.value;
     const rentInput = document.getElementById('rent-input');
-    const clothesInput = document.getElementById('clothes-input')
+    const rentInputValue = rentInput.value;
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesInputValue = clothesInput.value;
+    const expanceTotal = parseFloat(foodInputValue) + parseFloat(rentInputValue) + parseFloat(clothesInputValue);
+    const balanceTotal = parseFloat(incomeInputValue) - expanceTotal;
+
+    const expanceTotalDisplay = document.getElementById('total-expance');
+    expanceTotalDisplay.innerText = expanceTotal;
+    const balanceTotalDisplay = document.getElementById('total-balance');
+    balanceTotalDisplay.innerText = balanceTotal;
+    return balanceTotal;
+}
+
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    expanceBalanceCalculation();
+})
+document.getElementById('save-btn').addEventListener('click', function () {
+    const totalBalance = expanceBalanceCalculation();
+    const saveInput = document.getElementById('save-input');
+    const saveInputValue = saveInput.value;
+    console.log(saveInputValue);
+    console.log(typeof (saveInputValue));
 })
